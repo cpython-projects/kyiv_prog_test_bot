35b3c9d395acd847c6a3a60d48896926c160e079
mihailzav9 <7754755m@gmail.com>
2023-06-04 11:42:54 +0300

    add instructors functions

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask
 
 app = Flask(__name__)
+def instructors():
+    instructors_dict = {}
+    with open("instructor.txt", "r") as file:
+        for line in file:
+            instructor, courses = line.strip().split(":")
+            instructor = instructor.strip()
+            courses = courses.strip()
+
+            instructors_dict[instructor] = courses
+
+    return instructors_dict
 
 
 @app.route('/')
